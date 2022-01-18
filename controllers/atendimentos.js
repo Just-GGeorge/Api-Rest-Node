23f7417de2d4b88f0e8c9cb92812f0a62120e43c
Guilherme George <guilhermegeorge06@gmail.com>
2022-01-18 14:17:45 -0400

    Refatorando o código

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,11 +1,11 @@
 const Atendimento = require ('../models/atendimento-models')
+
 module.exports = app => {
     app.get('/atendimentos',(req, res) => res.send('Você está na rota do atendimento e realizando um get'))
 
     app.post('/atendimentos',(req,res) =>{
         const atendimento = req.body
-        Atendimento.adiciona(atendimento)
-        res.send('Você está na rota de atendimentos realizando um post')
+        Atendimento.adiciona(atendimento,res)
     })
 }
 
